6b6012b7ff45e7f7753dfdea72ca6e84a07b0c40
Michał Szałkowski <szalek@btbw.pl>
2016-02-03 23:41:38 +0100

    add id

--- src/main/java/pl/btbw/web/product/ProductController.java
+++ src/main/java/pl/btbw/web/product/ProductController.java
@@ -1,10 +1,12 @@
 package pl.btbw.web.product;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.btbw.core.common.Component;
 import pl.btbw.core.common.Link;
+import pl.btbw.core.product.core.Product;
 import pl.btbw.core.product.core.ProductRepository;
 
 import java.util.ArrayList;
@@ -21,41 +23,41 @@ public class ProductController {
 	@RequestMapping(value = {"/component/product"})
 	public List<Link> links() {
 		List<Link> links = new ArrayList<>();
-		links.add(new Link("http://localhost:8095/component/product/author"));
-		links.add(new Link("http://localhost:8095/component/product/brand"));
-		links.add(new Link("http://localhost:8095/component/product/info"));
-		links.add(new Link("http://localhost:8095/component/product/all"));
-		links.add(new Link("http://localhost:8095/component/product/all/MAP"));
+		links.add(new Link("http://localhost:8095/component/product/{id}/author"));
+		links.add(new Link("http://localhost:8095/component/product/{id}/brand"));
+		links.add(new Link("http://localhost:8095/component/product/{id}/info"));
+		links.add(new Link("http://localhost:8095/component/product/{id}/all"));
+		links.add(new Link("http://localhost:8095/component/product/{id}/all/MAP"));
 		return links;
 	}
 
-	@RequestMapping(value = {"/component/product/author"})
-	public Component componentAuthor() {
-		return productRepository.getOne(1L)
+	@RequestMapping(value = {"/component/product/{id}/author"})
+	public Component componentAuthor(@PathVariable("id") long id) {
+		return productRepository.getOne(id)
 				.componentProvider()
 				.authorProductComponent()
 				.buildOne();
 	}
 
-	@RequestMapping(value = {"/component/product/brand"})
-	public Component componentBrand() {
-		return productRepository.getOne(1L)
+	@RequestMapping(value = {"/component/product/{id}/brand"})
+	public Component componentBrand(@PathVariable("id") long id) {
+		return productRepository.getOne(id)
 				.componentProvider()
 				.brandProductComponent()
 				.buildOne();
 	}
 
-	@RequestMapping(value = {"/component/product/info"})
-	public Component componentInfo() {
-		return productRepository.getOne(1L)
+	@RequestMapping(value = {"/component/product/{id}/info"})
+	public Component componentInfo(@PathVariable("id") long id) {
+		return productRepository.getOne(id)
 				.componentProvider()
 				.infoProductComponent()
 				.buildOne();
 	}
 
-	@RequestMapping(value = {"/component/product/all"})
-	public List<Component> componentAll() {
-		return productRepository.getOne(1L)
+	@RequestMapping(value = {"/component/product/{id}/all"})
+	public List<Component> componentAll(@PathVariable("id") long id) {
+		return productRepository.getOne(id)
 				.componentProvider()
 				.brandProductComponent()
 				.infoProductComponent()
@@ -63,9 +65,9 @@ public class ProductController {
 				.buildList();
 	}
 
-	@RequestMapping(value = {"/component/product/all/MAP"})
-	public Map<String, Component> componentAllMap() {
-		return productRepository.getOne(1L)
+	@RequestMapping(value = {"/component/product/{id}/all/MAP"})
+	public Map<String, Component> componentAllMap(@PathVariable("id") long id) {
+		return productRepository.getOne(id)
 				.componentProvider()
 				.brandProductComponent()
 				.infoProductComponent()
